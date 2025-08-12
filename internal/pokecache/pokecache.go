30fbc1a06605336affd984dc0ee6c123a5a73280
bntrtm <git.bntrtm@tjbainter.com>
2025-08-12 14:01:34 -0500

    feat: add caching logic

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,73 @@
+package pokecache
+
+import (
+	"time"
+	"sync"
+	"fmt"
+	"encoding/json"
+)
+
+type cacheEntry struct {
+	createdAt	time.Time //time entry was created
+	val			[]byte //raw data we're caching
+}
+
+type Cache struct {
+	cachedEntries	map[string]cacheEntry
+	interval		time.Duration
+	mu				sync.Mutex
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	var addEntry cacheEntry
+	if err := json.Unmarshal(val, &addEntry); err != nil {
+		fmt.Println(err)
+	}
+
+	c.cachedEntries[key] = addEntry
+
+}
+
+func (c *Cache) Get(key string) (entryData []byte, found bool) {
+	val, ok := c.cachedEntries[key]
+	if !ok {
+		return nil, false
+	}
+
+	data, err := json.Marshal(val)
+	if err != nil {
+		return nil, false
+	}
+	return data, true
+}
+
+func (c *Cache) reapLoop() {
+	
+	ticker := time.NewTicker(c.interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <- ticker.C:
+			c.mu.Lock()
+			for key, val := range c.cachedEntries {
+				if time.Since(val.createdAt) > c.interval {
+					delete(c.cachedEntries, key)
+				}
+			}
+			c.mu.Unlock()
+		}
+	}
+}
+
+func NewCache(interval time.Duration) Cache {
+	cache := Cache{
+        cachedEntries: make(map[string]cacheEntry),
+        interval:      interval,
+        // mu is automatically initialized to its zero value
+    }
+	go cache.reapLoop()
+
+	return cache
+
+}
